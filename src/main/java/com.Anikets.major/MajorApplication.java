048dc9ecc22bfe85de2bc7a7bd50b5db7e521edc
AniketDhede <125856828+AniketDhede@users.noreply.github.com>
2024-08-10 15:50:50 +0530

    MajorApplication.java

---
+++ src/main/java/com.Anikets.major/MajorApplication.java
@@ -0,0 +1,15 @@
+package com.Anikets.major;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class MajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajorApplication.class, args);
+	}
+
+}  
