4786df80160f38a695003a425cedfab552c72c79
AniketDhede <125856828+AniketDhede@users.noreply.github.com>
2024-08-10 18:25:24 +0530

    CategoryRepository.java

---
+++ src/main/java/com.Anikets.major.repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.Anikets.major.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Anikets.major.model.Category;
+
+
+  public interface CategoryRepository extends JpaRepository<Category, Integer>{
+  
+  }
+ 
